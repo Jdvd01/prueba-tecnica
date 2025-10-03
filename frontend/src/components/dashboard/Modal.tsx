3d57b4e0805f1abdb783adc9f159d32e4033f2e7
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-10-03 11:38:31 -0400

    feat(frontend): Add dashboard modal to create users

---
+++ frontend/src/components/dashboard/Modal.tsx
@@ -0,0 +1,82 @@
+import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import type { UserData } from "@/types/user";
+import React, { useState } from "react";
+
+export function Modal() {
+	const [open, setOpen] = useState(false);
+	const [userData, setUserData] = useState<UserData>({
+		name: "",
+		email: "",
+	});
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		try {
+			console.log("fetch");
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setOpen(false);
+			setUserData({ name: "", email: "" });
+		}
+	};
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = e.target;
+		setUserData({ ...userData, [name]: value });
+	};
+
+	return (
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogTrigger asChild>
+				<Button variant="outline">Agregar usuario</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Registrar usuario</DialogTitle>
+				</DialogHeader>
+				<div className="grid gap-4">
+					<div className="grid gap-3">
+						<Label htmlFor="name">Name</Label>
+						<Input
+							id="name"
+							name="name"
+							placeholder="Jose Velasquez"
+							onChange={handleChange}
+							value={userData.name}
+						/>
+					</div>
+					<div className="grid gap-3">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							id="email"
+							name="email"
+							placeholder="ejemplo@gmail.com"
+							onChange={handleChange}
+							value={userData.email}
+						/>
+					</div>
+				</div>
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button variant="outline">Cancel</Button>
+					</DialogClose>
+					<Button type="submit" onClick={handleSubmit}>
+						Save changes
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
