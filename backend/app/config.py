d40646584f95c157aa596c048fb10def89355c7e
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-10-02 21:42:51 -0400

    feat(config): Add SQLAlchemy env variables

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,5 +3,7 @@ import os
 class Config:
     HOST = os.getenv("HOST", "127.0.0.1")
     PORT = int(os.getenv("PORT", 8000))
-    DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "t")
+    DEBUG = os.getenv("DEBUG", True)
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
